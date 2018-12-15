5c56b70057017931c9de580f961f547f3b9d2410
ajin <jgz8823@126.com>
2018-12-15 22:15:32 +0800

    pipfiles, kix initial

---
+++ Crawler/DutyFreeShopList.js
@@ -0,0 +1,11 @@
+[
+	{
+		"JapanKix": "https://www.kixdutyfree.jp/cn/",
+		"JapanFasola": https://www.fasola-shop.com/zh-TW/",
+		"SingaporeChangi: https://zh.ishopchangi.com/",
+		"ChinaSunrise: "",
+		koreaLotte: http://chn.lottedfs.com/kr
+		KoreaShilla: http://www.shilladfs.com/estore/kr/zh/?uiel=Desktop
+		TaiwanEverrich: https://www.everrich.com/tw/
+	}
+]
