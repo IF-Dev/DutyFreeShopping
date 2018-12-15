bc7c1702ff999a0e9d5dcafb43c1781f507415de
ajin <jgz8823@126.com>
2018-12-15 22:41:47 +0800

    add quotes

--- Crawler/DutyFreeShopList.js
+++ Crawler/DutyFreeShopList.js
@@ -3,7 +3,7 @@
 		"JapanKix": "https://www.kixdutyfree.jp/cn/",
 		"JapanFasola": "https://www.fasola-shop.com/zh-TW/",
 		"SingaporeChangi": "https://zh.ishopchangi.com/",
-		"ChinaSunrise: "",
+		"ChinaSunrise": "",
 		"koreaLotte": "http://chn.lottedfs.com/kr",
 		"KoreaShilla": "http://www.shilladfs.com/estore/kr/zh/?uiel=Desktop",
 		"TaiwanEverrich": "https://www.everrich.com/tw/"
